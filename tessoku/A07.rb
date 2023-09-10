05f07558b4c794b0fd31cc9bbef4fad6cc13b60f
Ito Masaki <msk.nfl.teabow12@gmail.com>
2023-09-10 23:46:01 +0900

    20230910

---
+++ tessoku/A07.rb
@@ -0,0 +1,13 @@
+ary = Array.new(gets.chomp.to_i + 1, 0)
+n = gets.chomp.to_i
+
+n.times do
+  first, last = gets.chomp.split.map(&:to_i)
+  ary[first - 1] += 1
+  ary[last] -= 1
+end
+
+sum = 0
+ary[..-2].each do |n|
+  puts sum += n
+end
