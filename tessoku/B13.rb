f3a745f296d5b9f34abea208e9443e03e3bb0e46
Ito Masaki <msk.nfl.teabow12@gmail.com>
2023-09-26 00:11:58 +0900

    20230926

--- tessoku/B13.rb
+++ tessoku/B13.rb
@@ -6,12 +6,11 @@ ruiseki = arys.map do |num|
   sum += num
 end
 
-count = 0
-ruiseki.unshift(0).each_with_index do |small, idx|
-  r = ruiseki.bsearch_index { |big| big - small > 50 }
+count = ruiseki.bsearch_index { |big| big > K }
+ruiseki.each_with_index do |small, idx|
+  r = ruiseki.bsearch_index { |big| (big - small) > K }
   r = N unless r
-  result = (r.to_i - (idx + 1))
-  count += (r.to_i - idx - 1)
+  count += r - idx - 1
 end
 
 p count
